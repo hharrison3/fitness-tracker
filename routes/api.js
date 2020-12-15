c17bd12fe484b2347c54d18c183a4454537c936d
Harbour Harrison <harb3gaucho@gmail.com>
2020-12-14 19:51:18 -0800

    updated routes

--- routes/api.js
+++ routes/api.js
@@ -1,52 +1,51 @@
 const router = require("express").Router();
 const Workout = require("../models/workouts.js");
-
+//route to create a workout in the database
 router.post("/api/workouts", (req, res) => {
   Workout.create({})
-    .then(dbWorkout => {
-      res.json(dbWorkout);
+    .then(result => {
+      res.json(result);
     })
     .catch(err => {
       res.json(err);
     });
 });
-
+//route to update a current workout by id
 router.put("/api/workouts/:id", ({ body, params }, res) => {
   Workout.findByIdAndUpdate(
     params.id,
     { $push: { exercises: body } },
-    // "runValidators" will ensure new exercises meet our schema requirements
     { new: true, runValidators: true }
   )
-    .then(dbWorkout => {
-      res.json(dbWorkout);
+    .then(result => {
+      res.json(result);
     })
     .catch(err => {
       res.json(err);
     });
 });
-
+// route to get all workouts in database 
 router.get("/api/workouts", (req, res) => {
   Workout.find()
-    .then(dbWorkouts => {
-      res.json(dbWorkouts);
+    .then(result => {
+      res.json(result);
     })
     .catch(err => {
       res.json(err);
     });
 });
-
+//route to get a set of data in a range
 router.get("/api/workouts/range", (req, res) => {
-  Workout.find({}).limit(7)
-    .then(dbWorkouts => {
-      console.log(dbWorkouts)
-      res.json(dbWorkouts);
+  Workout.find({}).limit(10)
+    .then(result => {
+      console.log(result)
+      res.json(result);
     })
     .catch(err => {
       res.json(err);
     });
 });
-
+// route to delete all workouts by user
 router.delete("/api/workouts", ({ body }, res) => {
   Workout.findByIdAndDelete(body.id)
     .then(() => {
